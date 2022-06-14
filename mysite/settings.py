8e3d7cfffc09cde6f52cd9019e0e662c48270db6
TrylMik <mikichomik@wp.pl>
2022-06-14 14:48:19 +0200

    asd

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from telnetlib import LOGOUT
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
